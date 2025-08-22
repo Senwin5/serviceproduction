59feec056fce9d1b54996f9d34ac8a947679b231
Godwin Umosen <umosengodwin568@gmail.com>
2025-08-22 16:19:19 +0100

    Services page

---
+++ lib/pages/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Services"),
+        backgroundColor: Colors.orange, // Optional: make it visible
+        elevation: 4,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xfff7f9fa),
+                    Color.fromARGB(255, 235, 237, 238),
+                  ],
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Hello, Maria',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          'images/post.jpg',
+                          height: 60,
+                          width: 55,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Which service do you need',
+                    style: TextStyle(
+                      color: Color(0xff284a79),
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // Adding a search bar to the service below
+                  SizedBox(height: 20.0),
+                  Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "How can i help you?",
+                        hintStyle: TextStyle(color: Colors.black45),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Color(0xff284a79),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
