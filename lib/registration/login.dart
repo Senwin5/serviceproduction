4088fef04faa1a396b33ae702e7f6555b9a32823
Godwin Umosen <umosengodwin568@gmail.com>
2025-09-19 15:38:27 +0100

    First Descriptions

--- lib/registration/login.dart
+++ lib/registration/login.dart
@@ -16,9 +16,42 @@ class _LoginState extends State<Login> {
         child: Column(
           children: [
             Image.asset("asset/register/signin.png"),
+            SizedBox(height: 30.0),
             Padding(
-              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-              child: TextField(decoration: InputDecoration(hintText: 'Email')),
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email:',
+                  hintStyle: TextStyle(color: Colors.black54, fontSize: 25.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password:',
+                  hintStyle: TextStyle(color: Colors.black54, fontSize: 25.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
