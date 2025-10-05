d136119e5850a87e7040c6bfbfcaf371c6ef6ef9
Senwin5 <godwinsenwin@outlook.com>
2025-10-05 09:30:56 +0100

    project fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:serviceproduction/registration/signup.';
+import 'package:serviceproduction/registration/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
